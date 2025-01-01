f41e4a45a331033c9c287daea30c23154a499b92
Erick Okal <erickokal@Okinja.local>
2025-01-01 21:13:28 +0300

    feat(networking): Added a private subnet

--- networking.tf
+++ networking.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.subnet_cidr
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "region" {
 variable "vpc_cidr" {
   type = string
 }
+
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.0.0.0/24"
+}
